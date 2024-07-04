46e638bd9bf41f2c2264d39f9a20c1fe306c9029
Amine El Kouhen <105490765+amineelkouhen@users.noreply.github.com>
2024-07-04 20:16:32 +0200

    Update output.tf

--- modules/aws/ns-public/output.tf
+++ modules/aws/ns-public/output.tf
@@ -1,3 +1,7 @@
 output "cluster_master_dns" {
   value = "${var.subdomain}.cluster.${var.hosted_zone}"
 }
+
+output "routing_policy" {
+  value = aws_route53_traffic_policy_instance.policy-instance
+}
